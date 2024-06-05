3a08d721838e83d41ba4007de30ac375a43d8bb4
ratigrdzelidze <164761160+ratigrdzelidze@users.noreply.github.com>
2024-06-05 20:30:04 +0400

    Add files via upload

---
+++ leqcia19/script.js
@@ -0,0 +1,41 @@
+// let myname = "rati"
+// let myhobby = "play games, watch movies, play football"
+// let yearofbirth = 2008
+// let currentyear = 2024
+
+// console.log('my name is' + myname + "my hobby is" + myhobby + "my year of birth is" + yearofbirth + "current year is" + currentyear)
+
+
+
+
+
+// let ricxvi =[5, 10, 15, 20, 25]
+// console.log(ricxvi[0] + ricxvi[1] + ricxvi[2] + ricxvi[3] + ricxvi[4]/5)
+
+
+ 
+
+
+// let sia=['jora', 'gela', 'varlama', 'almanaxi', 'badri']
+// sia.shift()
+// sia.push('jora')
+// console.log(sia[4])
+
+
+
+
+
+let myobject ={
+    firstname: 'rati',
+    lastname: 'grdzelidze',
+    Adress: 'tbilisi',
+    age: 15,
+    phonenumber: [
+     { first: 557875544,
+       second: 555055458
+     }
+
+    ]
+}
+
+console.log(myobject)
